c51c782ea28e5604860057e5da89eaa7e0a4da8a
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-27 22:33:45 -0300

    Habilitando o Cors

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,9 +3,9 @@ const express = require('express')
 module.exports = function(server){
     //API ROUTES
     const router = express.Router()
-    server.use('api', router)
+    server.use('/api', router)
 
     //TODO ROUTES
     const todoService = require('../api/todo/todo-service')
-    todoService.register(router, './todos')
+    todoService.register(router, '/todos')
 }
