70d25da1ed923696a79af2550e82681f0c18597a
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-29 23:33:19 -0300

    Consulta e exclusao TODO

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
 
-export default props =>(
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props =>{
+    const renderRowns = () =>{
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style="danger" icon="trash-o"
+                    onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+
+
+
+    return(
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descricao</th>
+                    <th>Acoes</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRowns()}
+            </tbody>
+        </table>
+
+    )
+}
